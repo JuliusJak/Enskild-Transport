d68c530b6ecbce3eedb2c531bee0f46851b147a2
JuliusJak <julius.bis03@gmail.com>
2023-02-23 13:15:09 +0100

    Initial commit

---
+++ src/main/java/com/example/enskildtransport/EnskildTransportApplication.java
@@ -0,0 +1,13 @@
+package com.example.enskildtransport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnskildTransportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnskildTransportApplication.class, args);
+	}
+
+}
